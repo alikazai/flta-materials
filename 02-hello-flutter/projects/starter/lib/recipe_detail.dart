63774e9097df89f08e87ffd38d5f0552fdf7877c
Ali Kazai <alik@digintus.com>
2021-10-17 03:22:20 +0100

    created first app

---
+++ 02-hello-flutter/projects/starter/lib/recipe_detail.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+  const RecipeDetail({Key? key, required this.recipe}) : super(key: key);
+
+  @override
+  _RecipeDetailState createState() {
+    return _RecipeDetailState();
+  }
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  int _sliderVal = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.label),
+      ),
+      body: SafeArea(
+          child: Column(children: <Widget>[
+        SizedBox(
+          height: 300,
+          width: double.infinity,
+          child: Image(image: AssetImage(widget.recipe.imageUrl)),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(widget.recipe.label, style: const TextStyle(fontSize: 18)),
+        // const Padding(
+        //   padding: EdgeInsets.all(16.0),
+        //   child: Text("hello World!",
+        //       style: TextStyle(
+        //         fontWeight: FontWeight.bold,
+        //         color: Colors.blue,
+        //         fontSize: 15,
+        //       )),
+        // ),
+        Expanded(
+            child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredient.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredient[index];
+                  return Text(
+                      "${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient.name}");
+                })),
+        Slider(
+          min: 1,
+          max: 10,
+          divisions: 10,
+          label: "${_sliderVal * widget.recipe.servings} servings",
+          value: _sliderVal.toDouble(),
+          onChanged: (newValue) {
+            setState(() {
+              _sliderVal = newValue.round();
+            });
+          },
+          activeColor: Colors.green,
+          inactiveColor: Colors.black,
+        )
+      ])),
+    );
+  }
+}
